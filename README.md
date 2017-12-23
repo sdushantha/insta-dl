f4164010c2bb23cd6fbc150051c22219f91cecea
lukakerr <lukakerr1@gmail.com>
2017-12-23 12:04:49 +1100

    Fix JSON and next page issues

--- README.md
+++ README.md
@@ -1,20 +1,13 @@
 # ![image](https://user-images.githubusercontent.com/27065646/31065811-6dd88f9c-a748-11e7-9d57-2e0ae062f413.jpg) insta-dl
 
-**Sorry to say this but insta-dl has stopped working. This is because Instagram removed the JSON data from their site :confused:**
-
 Download Instagram images and videos as a bulk from any user who is **public**. 
 
-I would like to say a big thanks to [TripleFrequency](https://github.com/TripleFrequency), who helped me fix the 20 download limit!
+I would like to say a big thanks to [TripleFrequency](https://github.com/TripleFrequency) and [lukakerr](https://github.com/lukakerr), who helped me fix the 20 image download limit!
+
 ## Installation
-```batch
+```bash
 git clone https://github.com/sdushantha/insta-dl.git
  ```
-
-## Requirements
-The module requests is needed for this program to work.
-```batch
-pip3 install requests
-```
 
 ## Contributing
 * Check any open issues or open a new issue to start discussions about your ideas of features and/or bugs
--- insta-dl.py
+++ insta-dl.py
@@ -1,73 +1,74 @@
 from time import sleep
-import requests, argparse, urllib.request, os, tkinter
-	
+import urllib, os, json
+try:
+  import tkinter
+except ImportError:
+  import Tkinter as tkinter
+  
 window = tkinter.Tk()
 
 do_download_videos = tkinter.IntVar(value=0)
 
 def download_images(username):
-	request_url = "https://www.instagram.com/"+username+"/media/"
-	
-	headers = {
-	'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0'
-	}
-	
-	params = {'max_id': '0'}
-	
-	more_available = True
-	
-	make_folder(entry.get())
-	
-	while more_available:
-		r = requests.get(request_url, headers=headers, params=params)
-		
-		try:
-			data = r.json()
-		except:
-			print("Invalid username!")
-			os.removedirs(entry.get())
-			break;
-		
-		for content in data["items"]:
-			if content["type"] == "video" and do_download_videos.get() == 1:
-				file_url = content["videos"]["standard_resolution"]["url"]
-			else:
-				file_url = content["images"]["standard_resolution"]["url"]
-				
-			file_url = file_url.replace("s640x640","s1080x1080")
-			
-			file_name = file_url.split("/")[-1]
-			
-			path = entry.get()+"/"+username+"_"+file_name
-			if not os.path.isfile(path):
-                                try:
-                                        urllib.request.urlretrieve(file_url,path)
-                                        print("Downloaded: "+path)
-                                        sleep(0.5)
-                                except:
-                                        print("----Skipping this image----")
-				
-		more_available = data["more_available"]
-		
-		new_max_id = data["items"][len(data["items"]) - 1]["id"]
-		
-		params = {'max_id': new_max_id}
-		
-		if more_available:
-			print("Getting next page of images with maximum id: "+new_max_id)
-		print("--------------Completed--------------")
-def action():
-        download_images(entry.get())
-        
-#Make folder with given username
-def make_folder(username):
+  request_url = "https://www.instagram.com/" + username + "?__a=1"
+  more_available = True
+  end_cursors = []
+  
+  make_folder(entry.get())
+  
+  while more_available:
+    if not end_cursors:
+      response = urllib.urlopen(request_url)
+    else:
+      response = urllib.urlopen(request_url + "&max_id={}".format(end_cursors[-1]))
+    
+    try:
+      data = json.loads(response.read())
+    except:
+      print("Invalid username!")
+      os.removedirs(entry.get())
+      break;
+    
+    for node in data["user"]["media"]["nodes"]:
+      src = node
+      # Cant access video url anymore
+      # if src["is_video"] and do_download_videos.get() == 1:
+      #   file_url = src["videos"]["standard_resolution"]["url"]
+      # else:
+      file_url = src["display_src"]
+      file_url = file_url.replace("s640x640","s1080x1080")
+      file_name = file_url.split("/")[-1]
+      
+      path = entry.get() + "/" + username + "_" + file_name
+      if not os.path.isfile(path):
         try:
-                os.makedirs(username)
-        except OSError:
-                os.removedirs(username)
-                os.makedirs(username)
+          urllib.urlretrieve(file_url,path)
+          print("Downloaded: "+path)
+          sleep(0.5)
+        except:
+          print("----Skipping this image----")
+        
+    more_available = data["user"]["media"]["page_info"]["has_next_page"]
+    
+    new_max_id = data["user"]["media"]["page_info"]["end_cursor"]
+    end_cursors.append(new_max_id)
+    
+    if more_available:
+      print("Getting next page of images with maximum id: " + new_max_id)
+    print("--------------Completed--------------")
 
-#Building the UI		
+def action():
+  download_images(entry.get())
+        
+# Make folder with given username
+def make_folder(username):
+  try:
+    os.makedirs(username)
+  except OSError:
+    os.removedirs(username)
+    os.makedirs(username)
+
+# Building the UI   
 window.configure(background="grey90")
 window.title("insta-dl v.0.2.4")
 window.geometry("300x200")
